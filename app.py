e658afbe35152bd0ae197f2e66e1afafdb5b1f95
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-09-17 12:19:11 +0000

    Added sign up page - work in progress

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, request
+from flask import Flask, render_template, redirect, request, url_for, request, flash, session
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 
 from os import path
@@ -16,13 +17,28 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/see_index')
-def see_index():
+@app.route('/show_index')
+def show_index():
     return render_template("index.html")
+
 
 @app.route('/sign_up')
 def sign_up():
     return render_template("sign_up.html")
+
+
+@app.route('/add_user', methods=['GET','POST'])
+def add_user():
+    users = mongo.db.users
+    users.insert_one(request.form.to_dict())
+    return redirect(url_for('user_account'))
+
+
+@app.route('/user_account', methods=['GET', 'POST'])
+def user_account():
+    users = mongo.db.users
+    return render_template("user_account.html", 
+                            users=mongo.db.users.find())
 
 
 @app.route('/get_trips')
