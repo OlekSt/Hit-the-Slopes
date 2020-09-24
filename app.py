f9e33652043afc43aaff8b4cbdf8c658123078d4
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-09-24 08:15:38 +0000

    Sign in & session works

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/show_index')
 def show_index():
-    return render_template("index.html")
+    if "user" not in session:
+        return render_template("index.html")
+    else: 
+        return render_template("trips.html", trips=mongo.db.trips.find(), active='signedIn')
 
 
 @app.route('/sign_up_page', methods=['GET', 'POST'])
@@ -49,30 +52,60 @@ def add_user():
         else:
             flash("This username already exists, please choose another one")
             return redirect(url_for('sign_up_page'))
-    return render_template('trips.html', active='signedIn')
+    return render_template('trips.html', active='signedIn', password=password)
+
+'''
+@app.route('/sign_in', methods=['GET', 'POST'])
+def sign_in():
+	if 'user' in session:
+		user_in_db = users.find_one({"name": session['user']})
+		if user_in_db:
+			# If so redirect user to his profile
+			flash("You are logged in already!")
+			return redirect(url_for('trips', user=user_in_db['name']))
+	else:
+		# Render the page for user to be able to log in
+		return render_template("sign_in.html")
+    
+
+@app.route('/user_auth', methods=['GET', 'POST'])
+def user_auth():
+    form = request.form.to_dict()
+    users = mongo.db.users.find()
+    user_in_db = users.find_one({"name": form["name"]})
+	if user_in_db:
+		if check_password_hash(user_in_db['password'], form['password']):
+			session['user'] = form['name']
+			
+			flash("You were logged in!")
+			return redirect(url_for('sign_in', user=user_in_db['name']))
+			
+		else:
+			flash("Wrong password or user name!")
+			return redirect(url_for('sign_in_page'))
+	else:
+		flash("You must be registered!")
+		return redirect(url_for('sign_up_page'))
+'''
 
 
 @app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
-    loggedIn = True if 'user' in session else False
-
     if request.method == "POST":
         users = mongo.db.users
         existing_user = users.find_one({'name': request.form["name"]})
+        
         if existing_user:
-            if check_password_hash(existing_user["password"], 
-                                    request.form["password"]):
+            if existing_user['password'] == request.form["password"]:
+                session["user"] = existing_user["name"]
                 flash("Welcome back, " + existing_user["name"])
-                session["name"] = existing_user["name"] 
                 return redirect(url_for('trips'))
-            
             else:
                 flash("Wrong password. Try again.")
                 return redirect(url_for('sign_in'))
         else:
             flash("Wrong name. Try again.")
             return redirect(url_for('sign_in'))
-
     return render_template('trips.html', active='signedIn')
 
 
@@ -83,17 +116,23 @@ def user_account():
 
 @app.route('/trips')
 def trips():
-    return render_template("trips.html", trips=mongo.db.trips.find())
+    if "user" not in session:
+        return redirect(url_for('sign_in_page'))
+    else:
+        return render_template("trips.html", trips=mongo.db.trips.find(), active='signedIn')
 
 
 @app.route('/ski_resorts')
 def ski_resorts():
-    return render_template("skiresorts.html", skiresorts=mongo.db.skiresorts.find())
+    if "user" not in session:
+        return redirect(url_for('sign_in_page'))
+    else: 
+        return render_template("skiresorts.html", skiresorts=mongo.db.skiresorts.find(), active='signedIn')
 
 
 @app.route('/sign_out')
 def sign_out():
-    session.clear()
+    [session.pop(key) for key in list(session.keys())]
     return redirect(url_for('show_index'))
 
 
